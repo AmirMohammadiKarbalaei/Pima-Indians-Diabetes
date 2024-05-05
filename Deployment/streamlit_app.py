165cc26c8eea31f68992a926278e9709e16ac509
AmirMohammadiKarbalaei <a.mohammadikarbalaei@gmail.com>
2024-05-05 15:26:09 +0100

    App changes

--- Deployment/streamlit_app.py
+++ Deployment/streamlit_app.py
@@ -5,19 +5,17 @@ from app_utils import *
 # Create the Streamlit web app
 def main():
     st.title('Diabetes Prediction')
-    st.write("This app predicts diabetes based on input data.")
+    st.write("This app predicts diabetes using  input data. Ensure that the input data includes the following features: ")
 
     # File uploader for CSV file
-    st.sidebar.header('Upload CSV File')
-    uploaded_file = st.sidebar.file_uploader("Upload your data as a CSV file (data needs to include Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction and patient's Age ", type=["csv"])
+    st.image("./Data-Table.PNG")
+    uploaded_file = st.file_uploader("Upload your data as a CSV file ", type=["csv"])
 
     if uploaded_file is not None:
         try:
         # Read the uploaded CSV file
             input_data = pd.read_csv(uploaded_file)
         
-
-
         # Show the uploaded data
             st.write('Uploaded CSV file:')
             st.write(input_data.head())
@@ -25,23 +23,23 @@ def main():
         
 
         # Make prediction when user clicks the 'Predict' button
-            if st.sidebar.button('Make Prediction'):
+            if st.button('Make Prediction'):
                 processed_data = preprocess(input_data)
                 prediction = predict(processed_data.values)
                 st.write('Prediction:')
                 st.write(prediction)
 
                 # Provide an option to save the prediction as a CSV file
-                st.sidebar.markdown('### Save Prediction')
-                st.sidebar.write('Click below to save the prediction as a CSV file.')
-                st.sidebar.download_button(label="Save Prediction as CSV",
+                st.markdown('### Save Prediction')
+                st.write('Click below to save the prediction as a CSV file.')
+                st.download_button(label="Save Prediction as CSV",
                                         data=pd.DataFrame(prediction, columns=["Prediction"]).to_csv(index=False),
                                         file_name='prediction.csv',
                                         mime='text/csv')
         except Exception as e:
             st.error(f"An error occurred: {e}")
 
-    if st.sidebar.button('Quit'):
+    if st.button('Quit'):
         st.stop()
 
 if __name__ == '__main__':
